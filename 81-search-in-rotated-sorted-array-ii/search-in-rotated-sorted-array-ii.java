b63ea160a49019bff1ea022f9d3b7a3c1146fb66
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2024-06-23 21:16:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (92.55%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        /*
+        Apporch 1: unsing sorting then use binary serach
         Arrays.sort(nums);
         int low=0;
         int high=nums.length-1;
@@ -7,7 +9,7 @@ class Solution {
         {
             int mid=(low+high)/2;
             if(nums[mid]==target) return true;
-
+            
             else if(nums[mid]<target)
             {
                 low=mid+1;
@@ -15,6 +17,52 @@ class Solution {
             else
             {
                 high=mid-1;
+            }
+        }
+        */
+
+        // Approach 2 : only using binary serach
+
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high])
+            {
+                low++;
+                high--;
+                continue;
+            }
+
+            //left sorted
+
+            if(nums[low]<=nums[mid])
+            {
+                if(nums[low]<=target && target<=nums[mid])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+                if(nums[mid]<=target && target<=nums[high])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
             }
         }
         return false;
