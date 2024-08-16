4c4319f83d48f19227e69f13c496327508b23c38
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2024-08-16 20:20:28 +0530

    Time: 1 ms (99.53%), Space: 56.4 MB (97.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,54 +1,36 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-		// ------------------------ this brute force apporach ---------------------------------------
-    //     int max=Integer.MIN_VALUE;
-    //     int n=nums.length;
-    //     for(int i=0;i<n;i++)
-		// 		{
-		// 		for(int j=i;j<n;j++)
-		// 		{
-		// 		int sum=0;
-		// 		for(int k=i;k<=j;k++)
-		// 		{
-		// 			sum+=nums[k];
-		// 		}
-		// 		max=Math.max(max,sum);
-		// 	}
-		// }
-		// return max;
+//         int largest=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=i;j<nums.length;j++)
+//             {
+//                 int sum=0;
+//                 for(int k=i;k<=j;k++)
+//                 {
+//                     sum=sum+nums[k];
 
-
-
-
-		// -------------------------kadanes alogrithm---------------------------------------------
-		
-				// we take leasr min valuese(-21535468  this VALUE was assumption)
-
-				int max=Integer.MIN_VALUE;
-				// this sum using was store or add the arr values
-				int sum=0;
-
-				for(int i=0;i<nums.length;i++)
-				{
-
-					// we are adding the numbers
-					sum+=nums[i];
-
-					//  checking the sum is greater than the max and we assign the "sum values to max "
-					// ------------------first approach------------------------- 
-
-					// if(sum>max)
-					// {
-					// 	max=sum;
-					// }
-					//------------------ second apporach ----------------------------
-              max=Math.max(max,sum);
-					// we checking the sum is negative or not 
-					if(sum<0)
-					{
-						sum=0;
-					}
-				}
-				return max;
+//                 }
+//                 largest=Math.max(sum,largest);
+//             }
+//         }
+//         return largest;
+        
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum=sum+nums[i];
+            if(sum>max)
+            {
+                max=sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return max;
+        
     }
 }
