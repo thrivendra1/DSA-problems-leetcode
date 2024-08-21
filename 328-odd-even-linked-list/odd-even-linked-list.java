7d22f0d85726cf9ea8249cccfb8fc597cdebe855
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2024-08-21 20:10:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (47.46%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -11,6 +11,14 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) 
     {
+
+        /* question expantion
+        this question doesn't focuns of data was even or odd or -ve or +ve , they foucus on index only 
+        first you have to store odd inedx values then even index vlaues 
+        TC-o(n)
+        
+
+        Apporch-1
         ListNode temp=head;
         ArrayList <Integer> oddarr=new ArrayList<>();
         ArrayList <Integer> evenarr=new ArrayList<>();
@@ -46,6 +54,28 @@ class Solution {
         }
         
 
+        return head;
+        */
+        if(head==null || head.next==null)
+            {
+                return head;
+            }
+
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode evenhead=head.next;
+       
+        while(even!=null && even.next!=null)
+        {
+            
+            odd.next=odd.next.next;
+            
+            even.next=even.next.next;
+            odd=odd.next;
+            even=even.next;
+            
+        }
+        odd.next=evenhead;
         return head;
         
     }
