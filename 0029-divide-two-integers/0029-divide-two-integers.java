701636a6642deb6096f9ad78432f1af1440a85da
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2024-09-09 20:45:06 +0530

    Time: 1 ms (71.7%), Space: 40.7 MB (63.57%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -2,17 +2,40 @@ class Solution {
     public int divide(int dividend, int divisor) {
         int n = 0;
         long c = 0;
-        long sign = 1;
-        if (divisor == 1) return dividend;
-        if ((dividend < 0 && divisor >= 0) || (dividend >= 0 && divisor < 0)) sign = -1;
+        boolean sign = true;
+        if (divisor == dividend) return 1;
+        if ((dividend < 0 && divisor >= 0) || (dividend >= 0 && divisor < 0))
+        {
+            sign = false;
+        } 
         long dd = Math.abs((long) dividend);
         long dr = Math.abs((long) divisor);
-        for (int i = 30; i >= 0; i--) {
-            if (dd >= (dr << i)) {
-                c += (1 << i);
-                dd -= (dr << i);
+        // for (int i = 30; i >= 0; i--) {
+        //     if (dd >= (dr << i)) {
+        //         c += (1 << i);
+        //         dd -= (dr << i);
+        //     }
+        // }
+        // return (int) (c * sign);
+        int ans=0;
+        while(dd>=dr)
+        {
+            int count=0;
+            while(dd>=(dr<<(count+1)))
+            {
+                count++;
             }
+            ans+=1<<count;
+            dd-=(dr<<count);
         }
-        return (int) (c * sign);
+        if(ans==(1<<31) && sign)
+        {
+            return Integer.MAX_VALUE;
+        }
+        if(ans==(1<<31) && !sign)
+        {
+            return Integer.MIN_VALUE;
+        }
+        return sign?ans:-ans;
     }
 }
