edebaf348e90a72ad2625c42ad051738b126677a
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2024-09-25 19:00:28 +0530

    Time: 3 ms (93.11%), Space: 44.3 MB (24.56%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,6 +1,8 @@
 class Solution {
+  /*  //Apporach -1 
     static   public int[] nextGreaterElement(int[] nums1, int[] nums2)
     {
+
         int [] result=new int[nums1.length];
 
         boolean flag;
@@ -39,4 +41,45 @@ class Solution {
         return result;
 
     }
+    */
+        static   public int[] nextGreaterElement(int[] nums1, int[] nums2)
+        {
+            //hashmap to store a num2 digit and there next greater element 
+
+            HashMap<Integer,Integer> map=new HashMap<>();
+
+            // Stack , for using monotonic Decreasing order
+
+            Stack<Integer> st=new Stack<>();
+
+            for(int i=nums2.length-1;i>=0;i--)
+            {
+                // monotonic decreasing code
+
+                while(!st.isEmpty() && st.peek()<=nums2[i])
+                {
+                    st.pop();
+                }
+
+                // storing nums2 data in stack
+
+                if(!st.isEmpty())
+                {
+                    map.put(nums2[i],st.peek());
+                }
+                else
+                {
+                    map.put(nums2[i],-1);
+                }
+                st.push(nums2[i]);
+            }
+
+            int result[]=new int[nums1.length];
+
+            for(int i=0;i<nums1.length;i++)
+            {
+                result[i]=map.get(nums1[i]);
+            }
+            return result;
+        }
 }
