66c22304528c79ee0f78741bbe1c0c3614ebdd7f
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2024-11-03 20:25:23 +0530

    Time: 1 ms (100%), Space: 47.6 MB (54.6%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int max=0;
+ /*       int max=0;
 
         for(int i=0;i<nums.length;i++)
         {
@@ -17,6 +17,32 @@ class Solution {
             }
         }
         return max;
+        */
+
+    // using sliding window
+
+    int ans=subarrays(nums,goal)-subarrays(nums,goal-1);
+    return ans;
         
+    }
+
+    static int subarrays(int []nums,int goal)
+    {
+        if(goal<0) return 0;
+        int sum=0,count=0,l=0,r=0;
+
+        while(r<nums.length)
+        {
+            sum=sum+nums[r];
+
+            while(sum>goal)
+            {
+                sum=sum-nums[l];
+                l++;
+            }
+            count=count+(r-l+1);
+            r++;
+        }
+        return count;
     }
 }
