27649316bcd4bbff0a955caead8a279a4750d261
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-02-06 20:56:47 +0530

    Time: 11 ms (6.22%), Space: 44.8 MB (5.72%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,74 @@
+class Solution {
+    static public List<List<Integer>> subsetsWithDup(int[] nums)
+    {
+
+        List<Integer> input=new ArrayList<>();
+        for(int n:nums)
+        {
+            input.add(n);
+        }
+        List<Integer> output=new ArrayList<>();
+        List<List<Integer>> result=new ArrayList<>();
+        uniquesubset(input,output,0,result);
+        System.out.print(result);
+
+
+        // Sorting by the first element
+
+        System.out.println("----------------------");
+
+        // Sorting each subset in ascending order
+        for(List<Integer> subset : result) {
+            Collections.sort(subset);
+        }
+
+        // Sorting the entire result based on subsets
+        result.sort((a, b) -> {
+            for (int i = 0; i < Math.min(a.size(), b.size()); i++) {
+                if (!a.get(i).equals(b.get(i))) {
+                    return a.get(i) - b.get(i); // Compare elements
+                }
+            }
+            return a.size() - b.size(); // If equal, smaller list first
+        });
+
+        result=remove_duplicates(result);
+
+        // Sorting each subset in ascending order
+
+        System.out.print(result);
+        return result;
+
+    }
+
+        static public List<List<Integer>> remove_duplicates(List<List<Integer>> result)
+        {
+            Set<List<Integer>> ans=new HashSet<>(result);
+            return new ArrayList<>(ans);
+        }
+
+    static public List<List<Integer>> uniquesubset(List<Integer> input,List<Integer> output,int index,List<List<Integer>> result)
+    {
+        //base
+
+        if(index>=input.size())
+        {
+            result.add(new ArrayList<>(output));
+
+            return result;
+        }
+
+        //exclude
+        uniquesubset(input,output,index+1,result);
+
+        //inculde
+
+        int element=input.get(index);
+        output.add(element);
+        uniquesubset(input,output,index+1,result);
+
+        output.remove(output.size()-1);
+        return result;
+
+    }
+}
