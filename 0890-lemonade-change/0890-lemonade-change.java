f4059470c7db95dc7f91142f73c19af9fe26d0c5
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-04-01 12:38:44 +0530

    Time: 2 ms (94.14%), Space: 56 MB (53.32%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,58 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) 
+    {
+        int fivebill=0;
+        int tenbill=0;
+        int tenwtybill=0;
+
+        for(int i=0;i<bills.length;i++)
+        {
+            if(bills[i]==5)
+            {
+                fivebill++;
+            }
+            else if(bills[i]==10)
+            {
+                tenbill++;
+                // int amount=bills[i];
+                if(fivebill>0)
+                {
+                    fivebill--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            else if(bills[i]==20)
+            {
+                tenwtybill++;
+                if(fivebill>0 && tenbill>0 )
+                {
+                    fivebill--;
+                    tenbill--;
+                }
+                else if(fivebill>=3)
+                {
+                    fivebill-=3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        if(fivebill <0 || tenbill<0)
+        {
+            return false;
+        }
+        return true;
+        
+    }
+}
