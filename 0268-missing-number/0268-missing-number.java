3f3454871094f2bb6e636ff503da13c46d95e753
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-08-03 17:17:59 +0530

    Time: 5 ms (25.02%), Space: 45.3 MB (71.3%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,55 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int size=nums.length;
-        int total=size*(size+1)/2;
-        int sum=0;
-        for(int i=0;i<size;i++)
+        // Arrays.sort(nums);
+        // int size=nums.length;
+        // // we clacute the array size to get answer
+        // int total=size*(size+1)/2;
+        // int sum=0;
+        // for(int i=0;i<size;i++)
+        // {
+        //     sum=sum+nums[i];
+        // }
+        // return total-sum;
+
+        if(nums.length==1 && nums[0]>0)
         {
-            sum=sum+nums[i];
+            return nums[0]-1;
         }
-        return total-sum;
-       
+         if(nums.length==1 && nums[0]==0)
+         {
+            return 1;
+         }
+        int max=Integer.MIN_VALUE;
+    
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(max<nums[i])
+            {
+                max=nums[i];
+            }
+            
+
+            set.add(nums[i]);
+        }
+
+        int count=0;
+        for(int i=0;i<=max;i++)
+        {
+            if(set.contains(i))
+            {
+                count++;
+            }
+            else
+            {
+                return i;
+            }
+        }
+
+        if(count==nums.length)
+        {
+            return max+1;
+        }
+       return -1;
     }
 }
