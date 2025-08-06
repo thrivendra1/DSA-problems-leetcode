fa82adaad67e060b48fd18327da1d5886cfc5735
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-08-06 13:44:04 +0530

    Time: 20 ms (10.89%) | Memory: 48.9 MB (98.43%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,43 +1,43 @@
 class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer,Integer> numbers=new HashMap<>();
-    //     int numbermax=Integer.MIN_VALUE;
-    //     for(int i=0;i<nums.length;i++)
-    //     {
-    //         if(numbers.containsKey(nums[i]))
-    //         {
-    //             numbers.put(nums[i],numbers.get(nums[i])+1);
-    //         }
-    //         else
-    //         {
-    //             numbers.put(nums[i],1);
-    //         }
-
-    //     numbermax=Math.max(numbermax,numbers.get(nums[i]));
-       
-    //     }
-
-    //    for(Map.Entry<Integer,Integer> data:numbers.entrySet())
-    //    {
-    //         if(data.getValue()==numbermax)
-    //         {
-    //             numbermax=data.getKey();
-    //             break;
-    //         }
-    //    }
-    //     return numbermax;
-
-        int majoritynumber=0;
-        int majoritynumbercount=0;
-    for(int num:nums)
-    {
-        numbers.put(num,numbers.getOrDefault(num,0)+1);
-        if(numbers.get(num)>majoritynumbercount)
+        int numbermax=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
         {
-            majoritynumber=num;
-            majoritynumbercount=numbers.get(num);
+            if(numbers.containsKey(nums[i]))
+            {
+                numbers.put(nums[i],numbers.get(nums[i])+1);
+            }
+            else
+            {
+                numbers.put(nums[i],1);
+            }
+
+        numbermax=Math.max(numbermax,numbers.get(nums[i]));
+       
         }
-    }
-    return majoritynumber;
+
+       for(Map.Entry<Integer,Integer> data:numbers.entrySet())
+       {
+            if(data.getValue()==numbermax)
+            {
+                numbermax=data.getKey();
+                break;
+            }
+       }
+        return numbermax;
+
+    //     int majoritynumber=0;
+    //     int majoritynumbercount=0;
+    // for(int num:nums)
+    // {
+    //     numbers.put(num,numbers.getOrDefault(num,0)+1);
+    //     if(numbers.get(num)>majoritynumbercount)
+    //     {
+    //         majoritynumber=num;
+    //         majoritynumbercount=numbers.get(num);
+    //     }
+    // }
+    // return majoritynumber;
     }
 }
