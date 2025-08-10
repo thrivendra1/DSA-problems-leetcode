8826d28e703dc25bd004a8f435564c07e14319f2
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-08-10 16:33:03 +0530

    Time: 1 ms (72.37%), Space: 45.6 MB (66.96%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,15 +2,18 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int arr[][]=new int [m][n];
-        // we add matrix array to another array called 'arr'
+        int [][]dupicatematrix=new int[m][n];
+
+
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-               arr[i][j]=matrix[i][j];
+                dupicatematrix[i][j]=matrix[i][j];
             }
         }
+
+
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -19,11 +22,12 @@ class Solution {
                 {
                     for(int k=0;k<n;k++)
                     {
-                        arr[i][k]=0;
+                        dupicatematrix[i][k]=0;
                     }
+
                     for(int k=0;k<m;k++)
                     {
-                        arr[k][j]=0;
+                        dupicatematrix[k][j]=0;
                     }
                 }
             }
@@ -32,10 +36,8 @@ class Solution {
         {
             for(int j=0;j<n;j++)
             {
-                matrix[i][j]=arr[i][j];
+                matrix [i][j]=dupicatematrix[i][j];
             }
         }
-
-        // return matrix;
     }
 }
