3c1b18c1a8d38211f86fd37c85c27979603a3f75
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-09-12 13:25:42 +0530

    Time: 0 ms (100%), Space: 42.5 MB (27.43%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,53 +1,68 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-           if(m==1 && n==0)
-           {
-                nums1[0]=nums1[0];
-           }
-           if(n==1 && m==0)
-           {
-            nums1[0]=nums2[0];
-           }
+        //    if(m==1 && n==0)
+        //    {
+        //         nums1[0]=nums1[0];
+        //    }
+        //    if(n==1 && m==0)
+        //    {
+        //     nums1[0]=nums2[0];
+        //    }
 
 
-        int l1=m,l2=n;
-        int right=nums1.length-1;
+        // int l1=m,l2=n;
+        // int right=nums1.length-1;
 
-        while(l1>0 && l2>0)
-        {
-            if(nums1[l1-1] > nums2[l2-1])
-            {
-                int temp=nums1[right];
-                nums1[right]=nums1[l1-1];
-                nums1[l1-1]=temp;
-                l1--;
-                right--;
-                
-            }
-            else if(nums2[l2-1]>= nums1[l1-1])
-            {
-                nums1[right]=nums2[l2-1];
-                l2--;
-                right--;
-            }
-        }
-        // while( l1>0)
+        // while(l1>0 && l2>0)
         // {
         //     if(nums1[l1-1] > nums2[l2-1])
         //     {
-        //         int temp=nums1[l1-1];
-        //         nums1[l1-1]=nums2[l2-1];
-        //         nums2[l2-1]=temp;
+        //         int temp=nums1[right];
+        //         nums1[right]=nums1[l1-1];
+        //         nums1[l1-1]=temp;
         //         l1--;
+        //         right--;
+                
+        //     }
+        //     else if(nums2[l2-1]>= nums1[l1-1])
+        //     {
+        //         nums1[right]=nums2[l2-1];
+        //         l2--;
+        //         right--;
         //     }
         // }
 
-        while(l2>0)
-        {
-             nums1[right]=nums2[l2-1];
-             l2--;
-             right--;
+        // while(l2>0)
+        // {
+        //      nums1[right]=nums2[l2-1];
+        //      l2--;
+        //      right--;
              
+        // }
+
+
+        int l1=m-1,l2=n-1;
+        int r=m+n-1;
+
+        while(l1>=0 && l2>=0)
+        {
+            if(nums1[l1]>nums2[l2])
+            {
+                nums1[r]=nums1[l1];
+                l1--;
+            }
+            else
+            {
+                nums1[r]=nums2[l2];
+                l2--;
+            }
+            r--;
+        }
+        while(l2>=0)
+        {
+            nums1[r]=nums2[l2];
+            l2--;
+            r--;
         }
     }
 }
