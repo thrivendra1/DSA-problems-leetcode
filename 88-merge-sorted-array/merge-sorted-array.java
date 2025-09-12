282055714b67dffbfbbd0d356594a8efbd4435b8
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-09-12 13:28:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (8.57%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -40,25 +40,25 @@ class Solution {
              
         // }
 
+            // assing two pointer approach using left and rigth
+        int l1=m-1,l2=n-1;  // left was using to check numbers which one is bigger
+        int r=m+n-1; // rigth was using to store the number
 
-        int l1=m-1,l2=n-1;
-        int r=m+n-1;
-
-        while(l1>=0 && l2>=0)
+        while(l1>=0 && l2>=0)  // Merge from the back while both arrays have elements left
         {
-            if(nums1[l1]>nums2[l2])
+            if(nums1[l1]>nums2[l2])  // chcekcing the nums1 are greater then nums2
             {
                 nums1[r]=nums1[l1];
                 l1--;
             }
-            else
+            else   // oppsite of the if condition
             {
                 nums1[r]=nums2[l2];
                 l2--;
             }
             r--;
         }
-        while(l2>=0)
+        while(l2>=0)  // adding the remaing numbers in nums2 array into nums1 
         {
             nums1[r]=nums2[l2];
             l2--;
