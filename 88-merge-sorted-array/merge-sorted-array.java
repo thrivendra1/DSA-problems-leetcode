092356b6f6e4c4d59f63a45b48a2ecac4e00747d
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-09-12 13:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (90.55%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,68 +1,65 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //    if(m==1 && n==0)
-        //    {
-        //         nums1[0]=nums1[0];
-        //    }
-        //    if(n==1 && m==0)
-        //    {
-        //     nums1[0]=nums2[0];
-        //    }
+ 
 
 
-        // int l1=m,l2=n;
-        // int right=nums1.length-1;
+        int l1=m-1,l2=n-1;
+        int right=m+n-1;
 
-        // while(l1>0 && l2>0)
-        // {
-        //     if(nums1[l1-1] > nums2[l2-1])
-        //     {
-        //         int temp=nums1[right];
-        //         nums1[right]=nums1[l1-1];
-        //         nums1[l1-1]=temp;
-        //         l1--;
-        //         right--;
+        while(l1>=0 && l2>=0)
+        {
+            if(nums1[l1] > nums2[l2])
+            {
                 
-        //     }
-        //     else if(nums2[l2-1]>= nums1[l1-1])
-        //     {
-        //         nums1[right]=nums2[l2-1];
-        //         l2--;
-        //         right--;
-        //     }
-        // }
+                nums1[right]=nums1[l1];
+               
+                l1--;
+                
+                
+            }
+            else 
+            {
+                nums1[right]=nums2[l2];
+                l2--;
+               
+            }
+                right--;
+        }
 
-        // while(l2>0)
-        // {
-        //      nums1[right]=nums2[l2-1];
-        //      l2--;
-        //      right--;
+        while(l2>=0)
+        {
+             nums1[right]=nums2[l2];
+             l2--;
+             right--;
              
-        // }
+        }
+
+
+        // ------------ the below code was same concelpt of the above but it has less code and remaing was same
 
             // assing two pointer approach using left and rigth
-        int l1=m-1,l2=n-1;  // left was using to check numbers which one is bigger
-        int r=m+n-1; // rigth was using to store the number
+        // int l1=m-1,l2=n-1;  // left was using to check numbers which one is bigger
+        // int r=m+n-1; // rigth was using to store the number
 
-        while(l1>=0 && l2>=0)  // Merge from the back while both arrays have elements left
-        {
-            if(nums1[l1]>nums2[l2])  // chcekcing the nums1 are greater then nums2
-            {
-                nums1[r]=nums1[l1];
-                l1--;
-            }
-            else   // oppsite of the if condition
-            {
-                nums1[r]=nums2[l2];
-                l2--;
-            }
-            r--;
-        }
-        while(l2>=0)  // adding the remaing numbers in nums2 array into nums1 
-        {
-            nums1[r]=nums2[l2];
-            l2--;
-            r--;
-        }
+        // while(l1>=0 && l2>=0)  // Merge from the back while both arrays have elements left
+        // {
+        //     if(nums1[l1]>nums2[l2])  // chcekcing the nums1 are greater then nums2
+        //     {
+        //         nums1[r]=nums1[l1];
+        //         l1--;
+        //     }
+        //     else   // oppsite of the if condition
+        //     {
+        //         nums1[r]=nums2[l2];
+        //         l2--;
+        //     }
+        //     r--;
+        // }
+        // while(l2>=0)  // adding the remaing numbers in nums2 array into nums1 
+        // {
+        //     nums1[r]=nums2[l2];
+        //     l2--;
+        //     r--;
+        // }
     }
 }
