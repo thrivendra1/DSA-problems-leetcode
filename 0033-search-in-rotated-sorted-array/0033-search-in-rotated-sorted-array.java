77e1bea43e44daa9e0d6544ab306d3ba8abab633
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-09-21 12:38:03 +0530

    Time: 0 ms (100%), Space: 42 MB (69.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,21 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length-1;
+        int high=nums.length-1;
         int low=0;
-        int high=n;
+
 
         while(low<=high)
         {
-            // int mid=low+(high-low)/2;
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
 
             if(nums[mid]==target)
             {
                 return mid;
             }
-            
+
+            // In normal array we can decided to move left or right 
+            // but this problem they say was distict so we have to check the target was in left side of right side
+
+            // checking the left to mid was sorted array
+
             if(nums[low]<=nums[mid])
             {
+                //we know the left array was sorted so we can check the targert weas in left side array
+
                 if(nums[low]<=target && target<=nums[mid])
                 {
                     high=mid-1;
@@ -27,7 +33,7 @@ class Solution {
             }
             else
             {
-                if(nums[mid]<=target && target<=nums[high])
+                if(target>=nums[mid] && target<=nums[high])
                 {
                     low=mid+1;
                 }
