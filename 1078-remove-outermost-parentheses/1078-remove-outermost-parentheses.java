c421e5288a8997095d2333b8871aaface08ca0cd
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-09-22 13:15:10 +0530

    Time: 4 ms (62.22%), Space: 42.9 MB (20.27%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,21 +1,33 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         StringBuilder sb=new StringBuilder("");
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('){
-                count++;
-                if(count>1){
-                    sb.append(s.charAt(i));
+
+        int openBreact=0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+
+            if(c=='(')
+            {
+                openBreact++;
+                if(openBreact>1)
+                {
+                    sb.append(c);
                 }
             }
-            else{
-                count--;
-                if(count>0){
-                    sb.append(s.charAt(i));
+
+            else
+            {
+                openBreact--;
+
+                if(openBreact>0)
+                {
+                    sb.append(c);
                 }
             }
         }
-        return sb.toString();
+
+            return sb.toString();
     }
 }
