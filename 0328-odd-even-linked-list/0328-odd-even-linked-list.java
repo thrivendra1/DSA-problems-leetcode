d7a449bfb2b550db8a2c924d1cb1659a799ccdfb
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-09-28 12:28:50 +0530

    Time: 0 ms (100%), Space: 44.4 MB (91.63%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -9,74 +9,26 @@
  * }
  */
 class Solution {
-    public ListNode oddEvenList(ListNode head) 
-    {
+    public ListNode oddEvenList(ListNode head) {
 
-        /* question expantion
-        this question doesn't focuns of data was even or odd or -ve or +ve , they foucus on index only 
-        first you have to store odd inedx values then even index vlaues 
-        TC-o(n)
-        
-
-        Apporch-1
-        ListNode temp=head;
-        ArrayList <Integer> oddarr=new ArrayList<>();
-        ArrayList <Integer> evenarr=new ArrayList<>();
-        int count=0;
-        while(temp!=null)
-        {
-            count++;
-            if(count%2!=0)
-            {
-                oddarr.add(temp.val);
-            }
-            else if(count%2==0)
-            {
-                evenarr.add(temp.val);
-            }
-            temp=temp.next;
-        }
-
-        //adding odd index in the Node
-            temp=head;
-        for(int i=0;i<oddarr.size();i++)
-        {
-            temp.val=oddarr.get(i);
-            temp=temp.next;
-
-        }
-        //adding even index in the Node
-        for(int i=0;i<evenarr.size();i++)
-        {
-            temp.val=evenarr.get(i);
-            temp=temp.next;
-
-        }
-        
-
-        return head;
-        */
-        if(head==null || head.next==null)
-            {
-                return head;
-            }
-
+        if(head==null || head.next==null) return head;
         ListNode odd=head;
         ListNode even=head.next;
+        
+        //Storeing the even head address
         ListNode evenhead=head.next;
-       
-        while(even!=null && even.next!=null)
+
+
+        while( even!=null && even.next!=null)
         {
-            
             odd.next=odd.next.next;
-            
             even.next=even.next.next;
+
             odd=odd.next;
             even=even.next;
-            
         }
+
         odd.next=evenhead;
         return head;
-        
     }
 }
