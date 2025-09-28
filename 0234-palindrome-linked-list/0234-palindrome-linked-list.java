127a8c25e169256785e7f3001224b28d09821edc
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-09-28 11:51:51 +0530

    Time: 5 ms (53.7%), Space: 68.1 MB (56.78%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,61 +9,90 @@
  * }
  */
 class Solution {
-    
-    public ListNode findmidNode(ListNode head)
-    {
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null && fast.next!=null)
-        {
-            slow=slow.next; //+1
-            fast=fast.next.next; // +2
-        }
-        return slow;
-    }
     public boolean isPalindrome(ListNode head) {
         
-        if(head==null && head.next==null)
-        {
-            return true;
-        }
-        // STEP 1: Find midnode
+        if(head==null || head.next==null) return true;
+
+        // step1: find the middle node
+        ListNode middleNode=findmiddlenode(head);
         
-        ListNode midNode=findmidNode(head);
-        
-        // STEP 2: Reverser 2nd half
-        
+
+        // /step 2 -> reverse the second half linkedlist
+
         ListNode prev=null;
-        ListNode current=midNode;
+        ListNode current=middleNode;
         ListNode next;
-        
-        
+
         while(current!=null)
         {
             next=current.next;
             current.next=prev;
-            
             prev=current;
             current=next;
         }
-        
-                    // System.out.print(current.val);
 
-        ListNode right=prev;
+        // step3 compare the two halfs
+
         ListNode left=head;
-        
-        //Step 3: check it's true or false
-        
-        while(right!=null)
+        ListNode right=prev;
+
+        while(left!=null && right!=null)
         {
-            if(left.val!=right.val)
-            {
-                return false;
-            }
+            if(left.val!=right.val) return false;
+
             left=left.next;
             right=right.next;
         }
+
         return true;
+    }
+
+
+    public static ListNode findmiddlenode(ListNode head)
+    {
+        ListNode ans;
+        ListNode temp=head;
+
+        int size=0;
+
+        while(temp!=null)
+        {
+            temp=temp.next;
+            size++;
+        }
         
+        ListNode temp1=head;
+        ListNode temp2=head;
+
+        if(size/2==0)
+        {
+           
+            size=size/2;
+            while(temp1!=null && size>0)
+            {
+                size--;
+                temp1=temp1.next;
+            }
+            ans=temp1;
+
+        }
+        else
+        {
+            size=(size/2)+1;
+
+            while(temp2!=null)
+            {
+                size--;
+                if(size<=0)
+                {
+                    break;
+                }
+                temp2=temp2.next;
+            }
+            ans=temp2;
+        }
+
+        return ans;
+
     }
 }
