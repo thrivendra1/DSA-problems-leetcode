99c6e6cb8093f69389b2457becf4cd9632249b70
thrivendra <145692414+thrivendra1@users.noreply.github.com>
2025-10-30 12:57:40 +0530

    Time: 8 ms (43.95%), Space: 40.1 MB (97.2%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,35 +1,12 @@
 class Solution {
     public int fib(int n) {
 
-        
-        // Apporeach-1 
         if(n<=1)
         {
             return n;
         }
-        int f1=0;
-        int f2=1;
-        int ans=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            ans=f1+f2;
-            f1=f2;
-            f2=ans;
-        }
-        return f1;
-        
 
-        // Apporach-2 we are using recursion
-
-        /*if(n<=1)
-        {
-            return n;
-        }
-        int flast=fib(n-1);
-        int slast=fib(n-2);
-        return flast+slast;
-        */
+        return fib(n-1)+fib(n-2);
         
     }
 }
